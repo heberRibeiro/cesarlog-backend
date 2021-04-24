bc2a4482088301f5fb270777a0af83621f4f55bc
Heber Ribeiro <55813932+heberRibeiro@users.noreply.github.com>
2021-04-24 18:39:29 -0300

    Project start

---
+++ src/main/java/com/unit/cesarlog/CesarlogApplication.java
@@ -0,0 +1,12 @@
+package com.unit.cesarlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CesarlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CesarlogApplication.class, args);
+	}
+}
---
+++ src/test/java/com/unit/cesarlog/CesarlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unit.cesarlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CesarlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

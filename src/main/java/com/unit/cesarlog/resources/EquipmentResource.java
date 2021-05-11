148de6645751dee92298043c09e0ad376858cc19
Heber Ribeiro <55813932+heberRibeiro@users.noreply.github.com>
2021-05-11 18:44:47 -0300

    Equipment by Id

--- src/main/java/com/unit/cesarlog/resources/EquipmentResource.java
+++ src/main/java/com/unit/cesarlog/resources/EquipmentResource.java
@@ -5,12 +5,14 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.unit.cesarlog.domain.Equipment;
 import com.unit.cesarlog.services.EquipmentService;
 
+import br.unit.pe.store.domain.Categoria;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 
@@ -27,6 +29,13 @@ public class EquipmentResource {
 	public ResponseEntity<List<Equipment>> findAll() {
 		List<Equipment> equipment = service.findAll();
 		return ResponseEntity.ok().body(equipment);
+	}
+	
+	@ApiOperation(value = "Busca um Equipamento pelo ID", notes = "Este endpoint faz a busca de uma Equipmento passando o Id como parâmetro da requisição.")
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Equipment> findById(@PathVariable Integer id) {
+		Equipment obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
 	}
 
 }
--- src/main/java/com/unit/cesarlog/services/EquipmentService.java
+++ src/main/java/com/unit/cesarlog/services/EquipmentService.java
@@ -1,12 +1,14 @@
 package com.unit.cesarlog.services;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.unit.cesarlog.domain.Equipment;
 import com.unit.cesarlog.repositories.EquipmentRepository;
+import com.unit.cesarlog.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class EquipmentService {
@@ -16,5 +18,16 @@ public class EquipmentService {
 	
 	public List<Equipment> findAll() {
 		return repository.findAll();
+	}
+	
+	public Equipment findById(Integer id) {
+
+		try {
+			Equipment equipment = repository.findById(id).get();
+			return equipment;
+
+		} catch (NoSuchElementException e) {
+			throw new ObjectNotFoundException("Categoria não encaontrada na base de dados.");
+		}
 	}
 }

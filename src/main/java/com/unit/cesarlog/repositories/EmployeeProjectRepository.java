3bf3387d88fea71184848ff66eca4c42a6ac461e
Heber Ribeiro <55813932+heberRibeiro@users.noreply.github.com>
2021-05-15 16:23:29 -0300

    Employee project service

--- src/main/java/com/unit/cesarlog/repositories/EmployeeProjectRepository.java
+++ src/main/java/com/unit/cesarlog/repositories/EmployeeProjectRepository.java
@@ -3,9 +3,9 @@ package com.unit.cesarlog.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.unit.cesarlog.domain.Branch;
+import com.unit.cesarlog.domain.EmployeeProject;
 
 @Repository
-public interface EmployeeProject extends JpaRepository<Branch, Integer>  {
+public interface EmployeeProjectRepository extends JpaRepository<EmployeeProject, Integer>  {
 
 }

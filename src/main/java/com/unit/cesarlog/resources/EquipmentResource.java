62ae8279cb44555cccc96a66e9fd5ad1f42f31e2
Heber Ribeiro <55813932+heberRibeiro@users.noreply.github.com>
2021-05-15 16:28:54 -0300

    Employee project resource

--- src/main/java/com/unit/cesarlog/resources/EquipmentResource.java
+++ src/main/java/com/unit/cesarlog/resources/EquipmentResource.java
@@ -5,7 +5,6 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -58,11 +57,13 @@ public class EquipmentResource {
 		return ResponseEntity.ok().body(equipment);
 	}
 	
-	@ApiOperation(value = "Deleta um Equipamento passando o ID como parâmetro da requisição")
-	@DeleteMapping(value = "/{id}")
-	public ResponseEntity<Void> delete(@PathVariable Integer id) {
-		service.delete(id);
-		return ResponseEntity.noContent().build();
-	}
+	/*
+	 * @ApiOperation(value =
+	 * "Deleta um Equipamento passando o ID como parâmetro da requisição")
+	 * 
+	 * @DeleteMapping(value = "/{id}") public ResponseEntity<Void>
+	 * delete(@PathVariable Integer id) { service.delete(id); return
+	 * ResponseEntity.noContent().build(); }
+	 */
 
 }

5a34510b5710dfd94b53bd7abcccade716ef5352
Heber Ribeiro <55813932+heberRibeiro@users.noreply.github.com>
2021-05-15 15:10:26 -0300

    Role repository

--- src/main/java/com/unit/cesarlog/repositories/RoleRepository.java
+++ src/main/java/com/unit/cesarlog/repositories/RoleRepository.java
@@ -3,9 +3,9 @@ package com.unit.cesarlog.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.unit.cesarlog.domain.Branch;
+import com.unit.cesarlog.domain.Role;
 
 @Repository
-public interface BranchRepository extends JpaRepository<Branch, Integer>  {
+public interface RoleRepository extends JpaRepository<Role, Integer>  {
 
 }
